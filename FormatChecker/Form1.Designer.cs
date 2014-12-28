067f77e91030ded1f55f73d618f503d5edd94f47
mjpournassari <mjpournassari@gmail.com>
2014-12-28 11:07:46 +0330

    First commit

---
+++ FormatChecker/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace FormatChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Param = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Min = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Max = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 50);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input File";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(172, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "DROP FILE HERE";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(8, 59);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(281, 249);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "VIDEO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(50, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "00:00:00.000";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Param,
+            this.Min,
+            this.Max,
+            this.Result});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(275, 230);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Param
+            // 
+            this.Param.HeaderText = "Param";
+            this.Param.Name = "Param";
+            this.Param.ReadOnly = true;
+            this.Param.Width = 75;
+            // 
+            // Min
+            // 
+            this.Min.HeaderText = "Min";
+            this.Min.Name = "Min";
+            this.Min.ReadOnly = true;
+            this.Min.Width = 50;
+            // 
+            // Max
+            // 
+            this.Max.HeaderText = "Max";
+            this.Max.Name = "Max";
+            this.Max.ReadOnly = true;
+            this.Max.Width = 50;
+            // 
+            // Result
+            // 
+            this.Result.HeaderText = "Result";
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.Width = 90;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.dataGridView2);
+            this.groupBox3.Location = new System.Drawing.Point(8, 314);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(281, 140);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "AUDIO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(50, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00:00:00.000";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Enabled = false;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(275, 121);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Param";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 75;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Min";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Max";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            this.dataGridViewTextBoxColumn3.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Result";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.Width = 90;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::FormatChecker.Properties.Resources._111;
+            this.pictureBox1.Location = new System.Drawing.Point(198, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(292, 460);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;           
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Format Checker V1.0 2013-12-19";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Param;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Min;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Max;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Result;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
+
---
+++ FormatChecker/Form1.cs
@@ -0,0 +1,243 @@
+﻿using MediaInfoNET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+
+using System.Text;
+
+using System.Windows.Forms;
+
+namespace FormatChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;
+            this.DragEnter += Form1_DragEnter;
+            //   this.DragDrop += Form1_DragDrop;
+        }
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+            textBox1.BackColor = Color.LightSkyBlue;
+            textBox1.Text = "DROP FILE HERE";
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filePaths = (string[])(e.Data.GetData(DataFormats.FileDrop));
+                if (Path.GetExtension(filePaths[0].ToString()).ToLower() == ".mp4")
+                {
+                    textBox1.Text = filePaths[0].ToString();
+                    textBox1.BackColor = Color.LightGreen;
+                    Checker();
+                }
+                else
+                {
+                    textBox1.Text = "FILE IS NOT.MP4";
+                    textBox1.BackColor = Color.Red;
+                }
+            }
+        }
+
+        protected void Checker()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+
+
+            MediaFile aviFile = new MediaFile(textBox1.Text);
+
+
+            //Video
+            label1.Text = aviFile.Video[0].DurationStringAccurate;
+
+            dataGridView1.Rows.Add("Format",
+                System.Configuration.ConfigurationSettings.AppSettings["VideoFormat"].Trim(),
+                "---",
+                aviFile.Video[0].Format);
+
+            //Check Format Row:
+            if (aviFile.Video[0].Format.ToLower() != System.Configuration.ConfigurationSettings.AppSettings["VideoFormat"].Trim().ToLower())
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.LightPink;
+            }
+            else
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.White;
+            }
+
+
+
+            string VBitrateMode = "Constant";
+
+            aviFile.Video[0].Properties.TryGetValue("Frame rate mode", out VBitrateMode);
+
+            if (VBitrateMode == "Variable")
+            {
+                string VBitrateVbrMax = "";
+                aviFile.Video[0].Properties.TryGetValue("Maximum bit rate", out VBitrateVbrMax);
+                dataGridView1.Rows.Add("Bitrate Max", "", "", VBitrateVbrMax);
+
+                string VBitrateVbrMin = "";
+                aviFile.Video[0].Properties.TryGetValue("Bit rate", out VBitrateVbrMin);
+                dataGridView1.Rows.Add("Bitrate Min", "", "", VBitrateVbrMin);
+            }
+            else
+            {
+                dataGridView1.Rows.Add("Bitrate",
+                    System.Configuration.ConfigurationSettings.AppSettings["VideoBitrateMin"].Trim(),
+                    System.Configuration.ConfigurationSettings.AppSettings["VideoBitrateMax"].Trim(),
+                    aviFile.Video[0].Bitrate);
+
+                //Check Bitrate Row:
+                if (!(aviFile.Video[0].Bitrate <= int.Parse(System.Configuration.ConfigurationSettings.AppSettings["VideoBitrateMax"].Trim())
+                    &&
+                    aviFile.Video[0].Bitrate >= int.Parse(System.Configuration.ConfigurationSettings.AppSettings["VideoBitrateMin"].Trim())))
+                {
+                    dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.LightPink;
+                }
+                else
+                {
+                    dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.White;
+                }
+
+
+                VBitrateMode = "Constant";
+            }
+
+            dataGridView1.Rows.Add("Frame Mode",
+                System.Configuration.ConfigurationSettings.AppSettings["VideoFrameMode"].Trim(),
+                "---",
+                VBitrateMode);
+
+            if (VBitrateMode.ToLower() != System.Configuration.ConfigurationSettings.AppSettings["VideoFrameMode"].Trim().ToLower())
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.LightPink;
+            }
+            else
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.White;
+            }
+
+
+            dataGridView1.Rows.Add("FormatID",
+                System.Configuration.ConfigurationSettings.AppSettings["VideoFormatID"].Trim(),
+                "---",
+                aviFile.Video[0].FormatID);
+
+            //Check Video Format Id Row:
+            if (aviFile.Video[0].FormatID.ToLower() != System.Configuration.ConfigurationSettings.AppSettings["VideoFormatID"].Trim().ToLower())
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.LightPink;
+            }
+            else
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.White;
+            }
+
+
+            dataGridView1.Rows.Add("FrameRate",
+                System.Configuration.ConfigurationSettings.AppSettings["VideoFrameRateMin"].Trim(),
+                System.Configuration.ConfigurationSettings.AppSettings["VideoFrameRateMax"].Trim(),
+                aviFile.Video[0].FrameRate);
+            //Check VideoBitrate
+            if (!(aviFile.Video[0].FrameRate <= double.Parse(System.Configuration.ConfigurationSettings.AppSettings["VideoFrameRateMax"].Trim())
+                   &&
+                   aviFile.Video[0].FrameRate >= double.Parse(System.Configuration.ConfigurationSettings.AppSettings["VideoFrameRateMin"].Trim())))
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.LightPink;
+            }
+            else
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.White;
+            }
+
+            dataGridView1.Rows.Add("Height",
+                 System.Configuration.ConfigurationSettings.AppSettings["VideoHeightMin"].Trim(),
+                 System.Configuration.ConfigurationSettings.AppSettings["VideoHeightMax"].Trim(),
+                aviFile.Video[0].Height);
+            //Check Height Row:
+            if (!(aviFile.Video[0].Height <= int.Parse(System.Configuration.ConfigurationSettings.AppSettings["VideoHeightMax"].Trim())
+                &&
+                aviFile.Video[0].Height >= int.Parse(System.Configuration.ConfigurationSettings.AppSettings["VideoHeightMin"].Trim())))
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.LightPink;
+            }
+            else
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.White;
+            }
+
+            dataGridView1.Rows.Add("Width",
+                 System.Configuration.ConfigurationSettings.AppSettings["VideoWidthMin"].Trim(),
+                 System.Configuration.ConfigurationSettings.AppSettings["VideoWidthMax"].Trim(),
+                aviFile.Video[0].Width);
+
+            //Check Video Width Row:
+            if (!(aviFile.Video[0].Width <= int.Parse(System.Configuration.ConfigurationSettings.AppSettings["VideoWidthMax"].Trim())
+              &&
+              aviFile.Video[0].Width >= int.Parse(System.Configuration.ConfigurationSettings.AppSettings["VideoWidthMin"].Trim())))
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.LightPink;
+            }
+            else
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.White;
+            }
+
+            dataGridView1.Rows.Add("IsInterlaced",
+                System.Configuration.ConfigurationSettings.AppSettings["VideoIsInterlaced"].Trim(),
+                "---",
+                aviFile.Video[0].IsInterlaced);
+            //Check Video IsInterlaced Row:
+            if (aviFile.Video[0].IsInterlaced.ToString().ToLower() != System.Configuration.ConfigurationSettings.AppSettings["VideoIsInterlaced"].Trim().ToLower())
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.LightPink;
+            }
+            else
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Style.BackColor = Color.White;
+            }
+
+
+            dataGridView1.ClearSelection();
+
+
+
+            //Audio
+            if (aviFile.Audio.Count > 0)
+            {
+                label2.Text = aviFile.Audio[0].DurationStringAccurate;
+                dataGridView2.Rows.Add("Format", "", "", aviFile.Audio[0].Format);
+                dataGridView2.Rows.Add("Bitrate", "", "", aviFile.Audio[0].Bitrate);
+                dataGridView2.Rows.Add("SamplingRate", "", "", aviFile.Audio[0].SamplingRate);
+                dataGridView2.Rows.Add("Channel(s)", "", "", aviFile.Audio[0].Properties["Channel(s)"].ToString());
+            }
+
+            dataGridView2.ClearSelection();
+        }
+
+
+    }
+}
